42a850b48981dff22400208098eb62b5aecb536f
Yau Goh Chow <yaugoh.chow@gmail.com>
2022-07-10 10:11:05 +0800

    Created database migrations

---
+++ readme.md
@@ -0,0 +1,27 @@
+# Database Migrations
+
+## My Garden Patch Database
+
+To Add a new Migration:
+
+```
+dotnet ef migrations add <name_of_migration> -s .\src\MyGardenPatch.Webapi\ -p .\src\MyGardenPatch.SqlServer\ --context MyGardenPatch.SqlServer.MyGardenPatchDbContext
+```
+
+To Update the Database:
+``` 
+dotnet ef database update -s .\src\MyGardenPatch.Webapi\ -p .\src\MyGardenPatch.SqlServer\ --context MyGardenPatch.SqlServer.MyGardenPatchDbContext
+```
+
+## Local Identity Database
+
+To Add a new Migration:
+
+```
+dotnet ef migrations add <name_of_migration> -s .\src\MyGardenPatch.Webapi\ -p .\src\MyGardenPatch.LocalIdentity\ --context MyGardenPatch.LocalIdentity.LocalIdentityDbContext
+```
+
+To Update the Database:
+``` 
+dotnet ef database update -s .\src\MyGardenPatch.Webapi\ -p .\src\MyGardenPatch.LocalIdentity\ --context MyGardenPatch.LocalIdentity.LocalIdentityDbContext
+```
---
+++ src/MyGardenPatch.SqlServer/Migrations/20220710012414_Initial.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyGardenPatch.SqlServer.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GardenBeds",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    GardenId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUri = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GardenBeds", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gardens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUri = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gardens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    RegisteredAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ReceivesEmail = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Plant",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUri = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GardenBedId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plant", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Plant_GardenBeds_GardenBedId",
+                        column: x => x.GardenBedId,
+                        principalTable: "GardenBeds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GardenBeds_GardenId",
+                table: "GardenBeds",
+                column: "GardenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GardenBeds_UserId",
+                table: "GardenBeds",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gardens_UserId",
+                table: "Gardens",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plant_GardenBedId",
+                table: "Plant",
+                column: "GardenBedId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_EmailAddress",
+                table: "Users",
+                column: "EmailAddress");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Gardens");
+
+            migrationBuilder.DropTable(
+                name: "Plant");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "GardenBeds");
+        }
+    }
+}
